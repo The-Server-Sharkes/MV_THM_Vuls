030d60e0d05cd84f105c360bdce2ae2e29c876c1
MartiFO <fornielesmarti@gmail.com>
2025-04-05 16:31:12 +0200

    desactivar icmp echo...

--- prepararMaquina/configuraciones/reglasFirewall.sh
+++ prepararMaquina/configuraciones/reglasFirewall.sh
@@ -10,6 +10,7 @@ iptables -F
 iptables -A INPUT -i $interfaz -p tcp --dport 22 -j ACCEPT
 #Reglas servidor web
 iptables -A INPUT -i $interfaz -p tcp --dport 80 -j ACCEPT
+#iptables -A INPUT -i $interfaz -p icmp --icmp-type echo-request -j DROP
 
 
 iptables -S
